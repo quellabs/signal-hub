4fa1bc267b5a54fea59d6563ce797eb7d10b2197
noescom <E934BFdkKtWndW7>
2025-05-14 11:13:47 +0200

    Implemented sculpt commands using new package #2

--- src/Sculpt/ServiceProvider.php
+++ src/Sculpt/ServiceProvider.php
@@ -13,45 +13,30 @@
 	class ServiceProvider extends \Quellabs\Sculpt\ServiceProvider {
 		
 		/**
-		 * @var array List of command class references to be registered with the application
-		 */
-		protected array $commands;
-		
-		/**
-		 * Initialize the service provider with available commands
-		 * Prepares an array of fully qualified class names for all CLI commands
-		 * that will be registered when the service provider boots.
-		 */
-		public function __construct() {
-			$this->commands = [
-				\Quellabs\ObjectQuel\Sculpt\Commands\MakeEntityCommand::class,
-				\Quellabs\ObjectQuel\Sculpt\Commands\MakeEntityFromTableCommand::class,
-				\Quellabs\ObjectQuel\Sculpt\Commands\MakeMigrationsCommand::class
-			];
-		}
-		
-		/**
 		 * Register all ObjectQuel commands with the Sculpt application
 		 * @param Application $app The Sculpt application instance
 		 */
 		public function register(Application $app): void {
-			$this->commands($app, $this->commands);
+			$this->commands($app, [
+				\Quellabs\ObjectQuel\Sculpt\Commands\MakeEntityCommand::class,
+				\Quellabs\ObjectQuel\Sculpt\Commands\MakeEntityFromTableCommand::class,
+				\Quellabs\ObjectQuel\Sculpt\Commands\MakeMigrationsCommand::class
+			]);
 		}
 		
 		/**
 		 * Boot the service provider and configure all commands
 		 * @param Application $app The Sculpt application instance
 		 * @return void
-		 * @throws OrmException If the configuration cannot be loaded
 		 */
 		public function boot(Application $app): void {
 			parent::boot($app);
 			
 			$configuration = $this->getConfiguration();
 			
-			foreach($this->commands as $command) {
-				$command->boot($configuration);
-			}
+			$app->getCommand('make:entity')->boot($configuration);
+			$app->getCommand('make:entity-from-table')->boot($configuration);
+			$app->getCommand('make:migrations')->boot($configuration);
 		}
 		
 		/**
