d5e44bc056c088d9247afca50a29a438e79bf9ab
noescom <E934BFdkKtWndW7>
2025-05-19 11:14:47 +0200

    Removed AbstractProvider

--- src/Provider/ProviderInterface.php
+++ src/Provider/ProviderInterface.php
@@ -9,4 +9,11 @@
 		 * @return array<string> Array of service names or class names
 		 */
 		public function provides(): array;
+		
+		/**
+		 * This method can be overridden to conditionally load providers
+		 * based on runtime conditions.
+		 * @return bool
+		 */
+		public function shouldLoad(): bool;
 	}
