2f2b019a23453e2c0df9ed60d019095d4cc32260
noescom <E934BFdkKtWndW7>
2025-05-19 11:12:11 +0200

    Removed function register from ProviderInterface

--- src/Provider/ProviderInterface.php
+++ src/Provider/ProviderInterface.php
@@ -5,13 +5,6 @@
 	interface ProviderInterface {
 
 		/**
-		 * Register the provider with a container
-		 * @param mixed $container The service container
-		 * @return void
-		 */
-		public function register(mixed $container): void;
-		
-		/**
 		 * Get the services provided
 		 * @return array<string> Array of service names or class names
 		 */
