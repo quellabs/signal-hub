b59a8ae23dd11e192b5cf97b1a785b3dc3aa3b7f
noescom <E934BFdkKtWndW7>
2025-05-25 16:16:22 +0200

    Added ServiceProviderInterface

---
+++ src/DependencyInjection/ServiceProviderInterface.php
@@ -0,0 +1,27 @@
+<?php
+	
+	namespace Quellabs\Contracts\DependencyInjection;
+	
+	use Quellabs\Contracts\Discovery\ProviderInterface;
+	
+	/**
+	 * Interface for service providers that support centralized autowiring
+	 */
+	interface ServiceProviderInterface extends ProviderInterface {
+		
+		/**
+		 * Determine if this provider supports creating the given class
+		 * @param string $className
+		 * @param array $metadata
+		 * @return bool
+		 */
+		public function supports(string $className, array $metadata): bool;
+		
+		/**
+		 * Create an instance of the class with pre-resolved dependencies
+		 * @param string $className The class to instantiate
+		 * @param array $dependencies Pre-resolved constructor dependencies
+		 * @return object
+		 */
+		public function createInstance(string $className, array $dependencies): object;
+	}
