52a167e76ec64cda6491273ce30d2324f1432466
noescom <E934BFdkKtWndW7>
2025-06-02 11:17:36 +0200

    Added composer post install script

---
+++ config/smarty.php
@@ -0,0 +1,10 @@
+<?php
+	
+	return [
+		'template_dir'   => dirname(__FILE__) . '/../templates/',
+		'compile_dir'    => dirname(__FILE__) . '/../storage/smarty/compile/',
+		'cache_dir'      => dirname(__FILE__) . '/../storage/smarty/cache/',
+		'debugging'      => false,
+		'caching'        => true,
+		'clear_compiled' => true,
+	];
