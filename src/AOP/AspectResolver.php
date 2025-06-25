083d9b46c7ba4b7e611d28acf2953b622d700f6d
noescom <E934BFdkKtWndW7>
2025-06-25 15:19:30 +0200

    Added try/catch to resolve

--- src/AOP/AspectResolver.php
+++ src/AOP/AspectResolver.php
@@ -29,34 +29,37 @@
 		 * @param object|string $controller The controller instance
 		 * @param string $method The method name being called
 		 * @return array Array of aspect annotation instances
-		 * @throws ParserException
 		 */
 		public function resolve(object|string $controller, string $method): array {
-			// Get all aspect annotations
-			$allAnnotations = $this->getAspectAnnotations($controller, $method);
-			
-			// Convert annotation instances to actual aspect objects with their parameters
-			$aspects = [];
-			
-			foreach ($allAnnotations as $annotation) {
-				// Extract the aspect class name from the 'value' parameter
-				// This comes from @InterceptWith(CacheAspect::class, ttl=300)
-				$aspectClass = $annotation->getInterceptClass(); // e.g., CacheAspect::class
+			try {
+				// Get all aspect annotations
+				$allAnnotations = $this->getAspectAnnotations($controller, $method);
+
+				// Convert annotation instances to actual aspect objects with their parameters
+				$aspects = [];
 				
-				// Get all annotation parameters except 'value' to pass to aspect constructor
-				// For @InterceptWith(CacheAspect::class, ttl=300), this gives us ['ttl' => 300]
-				$parameters = array_filter($annotation->getParameters(), function ($key) {
-					return $key !== 'value';
-				}, ARRAY_FILTER_USE_KEY);
+				foreach ($allAnnotations as $annotation) {
+					// Extract the aspect class name from the 'value' parameter
+					// This comes from @InterceptWith(CacheAspect::class, ttl=300)
+					$aspectClass = $annotation->getInterceptClass(); // e.g., CacheAspect::class
+					
+					// Get all annotation parameters except 'value' to pass to aspect constructor
+					// For @InterceptWith(CacheAspect::class, ttl=300), this gives us ['ttl' => 300]
+					$parameters = array_filter($annotation->getParameters(), function ($key) {
+						return $key !== 'value';
+					}, ARRAY_FILTER_USE_KEY);
+					
+					// Add the aspect class and parameters to the result list
+					$aspects[] = [
+						'class'      => $aspectClass,
+						'parameters' => $parameters
+					];
+				}
 				
-				// Add the aspect class and parameters to the result list
-				$aspects[] = [
-					'class'      => $aspectClass,
-					'parameters' => $parameters
-				];
+				return $aspects;
+			} catch (ParserException $e) {
+				return [];
 			}
-			
-			return $aspects;
 		}
 		
 		/**
