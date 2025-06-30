a677fcd7587bd7fbff5e99495716be6efcf22a00
noescom <E934BFdkKtWndW7>
2025-06-30 13:28:53 +0200

    publish() now returns void

--- src/Publishing/AssetPublisher.php
+++ src/Publishing/AssetPublisher.php
@@ -32,9 +32,9 @@
 		 * Publish the assets to the given base path
 		 * @param string $basePath The project root directory
 		 * @param bool $force Whether to overwrite existing files
-		 * @return bool True if publishing succeeded, false otherwise
+		 * @return void
 		 */
-		public function publish(string $basePath, bool $force = false): bool;
+		public function publish(string $basePath, bool $force = false): void;
 		
 		/**
 		 * Get instructions to show to the user after successful publishing
